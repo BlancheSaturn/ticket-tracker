e7b106b4f9171aeea3df2ab5613f4e89b043f9e0
Blanche Jarrett <blanchejarrett@gmail.com>
2022-05-09 14:46:57 +0100

    complete react setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.scss";
+import team from "./data/team";
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="greeting">
+        <h1 className="greeting__heading">Hello World</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
